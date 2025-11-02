492d8abe7dd328a7f26e729640fb78e750f2fd67
Sidney Leal <sidleal@gmail.com>
2025-11-02 09:08:10 -0300

    train prossegue

--- prepare_align.py
+++ prepare_align.py
@@ -2,7 +2,7 @@ import argparse
 
 import yaml
 
-from preprocessor import ljspeech, aishell3, libritts, nurc_min, mupev, cmltts, entoa_tts_pros, mupev2, cmltts_333, cmltts_entoa_pros, cmltts_entoa_auto
+from preprocessor import ljspeech, aishell3, libritts, nurc_min, mupev, cmltts, entoa_tts_pros, mupev2, cmltts_333, cmltts_entoa_pros, cmltts_entoa_auto, cmltts_entoa_prossegue
 
 
 def main(config):
@@ -18,6 +18,8 @@ def main(config):
         mupev.prepare_align(config)
     elif "cmltts_333" in config["dataset"]:
         cmltts_333.prepare_align(config)
+    elif "cmltts_entoa_prossegue" in config["dataset"]:
+        cmltts_entoa_prossegue.prepare_align(config)
     elif "cmltts_entoa_pros" in config["dataset"]:
         cmltts_entoa_pros.prepare_align(config)
     elif "cmltts_entoa_auto" in config["dataset"]:
